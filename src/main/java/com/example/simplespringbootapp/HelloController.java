0dab4baed28d8f94fa22982ff259b9e74307cdf5
khushi03112000 <khushimail03112000@gmail.com>
2025-04-27 14:53:49 +0530

    Add files via upload

---
+++ src/main/java/com/example/simplespringbootapp/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.simplespringbootapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "<h1>Hello from my simple Spring Boot Web App!</h1>";
+    }
+}
---
+++ src/main/java/com/example/simplespringbootapp/SimpleSpringbootAppApplication.java
@@ -0,0 +1,11 @@
+package com.example.simplespringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringbootAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringbootAppApplication.class, args);
+    }
+}
